cd04fc1f1cfb93070a7157a8d6af537e8c3cc30f
marcobusacca <marcobusacca01@gmail.com>
2023-07-05 14:51:12 +0200

    [FEAT] macrostruttura html & bootstrap cdn css

--- index.php
+++ index.php
@@ -9,8 +9,21 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap CDN CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <!-- Head Title -->
         <title>php-strong-password-generator</title>
     </head>
     <body>
+        <!-- Main -->
+        <main>
+            <!-- Main Container -->
+            <div class="container">
+                <!-- Main Row -->
+                <div class="row"></div>
+                </div>
+            </div>
+        </main>
+        <!-- Fine Main -->
     </body>
 </html>
