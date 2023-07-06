3629af5c98591567aa8dc8c481102bda2fa49a20
marcobusacca <marcobusacca01@gmail.com>
2023-07-06 17:29:17 +0200

    [FEAT] aggiunta polyfill

--- bonus/partials/functions.php
+++ bonus/partials/functions.php
@@ -1,5 +1,12 @@
 <!-- SCRIPT PHP -->
 <?php
+
+    if (!function_exists('str_contains')) {
+        function str_contains($haystack, $needle) {
+            return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+        }
+    }
+    
     // DEFINISCO LA FUNZIONE CHE GENERA UNA PASSWORD RANDOM
     function randomPassword($maxLength, $charactersRipetition) {
 
